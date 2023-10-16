fe1f354739086880277b4509cbe22b2ce1872598
davide-cas <casano.davide@outlook.it>
2023-10-16 09:57:55 +0200

    Genius API connection

---
+++ api_key.py
@@ -0,0 +1,3 @@
+client_id = "YOUR CLIENT ID"
+client_secret = "YOUR CLIENT SECRET"
+client_access_token = "YOUR ACCESS TOKEN"
---
+++ lyriclens.py
@@ -0,0 +1,56 @@
+from lyricsgenius import Genius
+import api_key
+import csv
+import json
+import re
+
+client_access_token = api_key.client_access_token
+genius = Genius(client_access_token)
+
+# Load the CSV with the songs
+file = open('songs.csv')
+csvreader = csv.reader(file)
+
+# Dictionary to store data by year
+song_data = {}
+
+with open("song_data.json", "w") as outfile:
+    for row in csvreader:
+        try:
+            year = row[0]
+            title = row[1]
+            artist = row[2]
+
+            # Get the song lyrics
+            song = genius.search_song(title, artist)
+
+            if song is None or song.lyrics == "":
+                continue
+
+            # Lyrics saving and splitting, avoinding "\n" keyword
+            lyrics = str(song.lyrics.split("\n",1)[1:])
+
+            if lyrics[0] == "[":
+                lyrics = lyrics[1:]
+            
+            lyrics = re.sub("[\[].*?[\]]", "", lyrics)
+
+            # Create a new song dictionary
+            new_song = {
+                "title": title,
+                "artist": artist,
+                "lyrics": lyrics
+            }
+
+            # Check if the year key exists in the dictionary, if not, create it
+            if year not in song_data:
+                song_data[year] = []
+
+            # Append the new song to the list of songs for the respective year
+            song_data[year].append(new_song)
+
+        except Exception as e:
+            print(e)
+
+    # Writing to song_data.json
+    json.dump(song_data, outfile, indent=4)
