7a16f86ec50ebd137c25508a27f2d9d038128ff4
davide-cas <casano.davide@outlook.it>
2023-10-19 11:25:17 +0200

    JSON reading through Apache Spark

--- main.py
+++ main.py
@@ -1,49 +1,40 @@
 import os, datetime
-# from databricks import koalas as ks
 from pyspark.sql.dataframe import DataFrame
 from pyspark.sql.types import *
 from pyspark.sql.functions import explode, split, col, sum, lit
 from pyspark.sql import SparkSession
+import sparknlp
+from sparknlp.pretrained import PretrainedPipeline
+from pyspark.sql.functions import col, explode
+from pyspark import SparkContext
+import json
 
-def apply_transforms(df: DataFrame) -> DataFrame:
+def load_songs(spark, json_path):
+  # Opening JSON file
+  f = open(json_path)
+  
+  # returns JSON object as a dictionary
+  data = json.load(f)
+  
+  df = spark.read.json(spark.sparkContext.parallelize([data]))
 
-    # split _c0 column as it is a string and we want the population data from it
-    split_col = split(df['_c0'], '\t')
+  # Iterate over the rows
+  rows = df.collect()
 
-    # add population column, group by country, sum population
-    return df \
-            .withColumn("population", split_col.getItem(2).cast('float')) \
-            .groupBy("country") \
-            .agg(col("country"), sum("population")).select(col("country"), col("sum(population)") \
-            .alias("population"))
+  for row in rows:
+      print(row[3])
+      #print(row[1][1].artist)
+      print(row[1][1].lyrics)
+      print(row[1][1].title)
+      print(type(row[0]))
 
 if __name__ == "__main__":
-    print("hello world")
-    # build spark session
-    # spark = SparkSession.builder.appName("KoalasPostgresDemo").getOrCreate()
+  spark = SparkSession.builder \
+      .appName("LyricsLens")\
+      .master("local[8]")\
+      .config("spark.driver.memory","16G")\
+      .config("spark.driver.maxResultSize", "0") \
+      .config("spark.kryoserializer.buffer.max", "2000M")\
+      .getOrCreate()
 
-    # # Enable hadoop s3a settings
-    # spark._jsc.hadoopConfiguration().set("com.amazonaws.services.s3.enableV4", "true")
-    # spark._jsc.hadoopConfiguration().set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
-    # spark._jsc.hadoopConfiguration().set("fs.s3a.aws.credentials.provider", \
-    # "com.amazonaws.auth.InstanceProfileCredentialsProvider,com.amazonaws.auth.DefaultAWSCredentialsProviderChain")
-    # spark._jsc.hadoopConfiguration().set("fs.AbstractFileSystem.s3a.impl", "org.apache.hadoop.fs.s3a.S3A")
-
-    # # read data from publc bucket into Spark DF
-    # data_path = "s3a://dataforgood-fb-data/csv/" 
-    # df = spark.read.csv(data_path)
-
-    # # apply spark transformations
-    # transformedDF = df.transform(apply_transforms)
-
-    # # build Koalas DF from Spark DF, get median, convert back to Spark DataFrame, add column with current date
-    # kDF = ks.DataFrame(transformedDF)
-    # medianDF = kDF.median().withColumn
-    # finalDF = medianDF.to_spark().withColumn("etl_time", lit(datetime.datetime.now()))
-
-    # # SQL metadata
-    # properties = {"user": os.environ['PG_USER'],"postgres": os.environ['PG_PASSWORD'],"driver": "org.postgresql.Driver"}
-    # url = f"jdbc:postgresql://{os.environ['PG_HOST']}:{os.environ['PG_PORT']}/{os.environ['PG_DB_NAME']}"
-
-    # # write to db
-    # finalDF.write.jdbc(url=url, table=os.environ['TABLE_NAME'], mode="overwrite", properties=properties)
+  load_songs(spark, json_path='songs/song_data.json')
